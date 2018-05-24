cc8241d1e330c847cee353194a7de7864ce3c00a
baggio10czm <baggio_tnt@163.com>
2018-05-24 23:16:45 +0800

    2018.0524 订单确认30%

---
+++ src/service/address-service.js
@@ -0,0 +1,25 @@
+/*
+* @Author: Rosen
+* @Date:   2017-05-27 18:26:52
+* @Last Modified by:   Rosen
+* @Last Modified time: 2017-05-29 16:54:03
+*/
+
+'use strict';
+
+var _mm = require('util/mm.js');
+
+var _address = {
+    // 获取地址列表
+    getAddressList : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/shipping/list.do'),
+            data:{
+              pageSize:50
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _address;
---
+++ src/service/order-service.js
@@ -0,0 +1,22 @@
+/*
+* @Author: Rosen
+* @Date:   2017-05-27 18:26:52
+* @Last Modified by:   Rosen
+* @Last Modified time: 2017-05-29 16:54:03
+*/
+
+'use strict';
+
+var _mm = require('util/mm.js');
+
+var _order = {
+    // 获取商品列表
+    getProductList : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/get_order_cart_product.do'),
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _order;
